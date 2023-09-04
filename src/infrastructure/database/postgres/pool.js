6f6fb1fb11c959ced4e699243860635a5e32862a
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-09-04 13:22:53 +0700

    chore: Initialize Commit

---
+++ src/infrastructure/database/postgres/pool.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const testConfig = {
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+};
+
+const pool =
+  process.env.NODE_ENV === "test" ? new Pool(testConfig) : new Pool();
+
+module.exports = pool;
